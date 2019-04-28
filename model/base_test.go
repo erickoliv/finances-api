d93e41ce3781f7f16c59d141f7dcf701d61d9cb0
Erick <contato@erick.net.br>
2019-04-27 20:37:23 -0400

    simple test file for base model

--- model/base_test.go
+++ model/base_test.go
@@ -1,1 +1,11 @@
 package model
+
+import "testing"
+
+func TestBaseModel_IsNew(t *testing.T) {
+	new := BaseModel{}
+
+	if !new.IsNew() {
+		t.Errorf("A zero-value model should return true.")
+	}
+}
