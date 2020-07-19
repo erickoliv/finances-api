a188767dd2e8f04af8f9f7567f80b7b4faa50fd0
Erick <contato@erick.net.br>
2020-07-19 16:47:47 -0300

    removed unused entities from deprecated domain package

--- domain/context.go
+++ domain/context.go
@@ -1,10 +1,6 @@
 package domain
 
 const (
-	// DB is used to pass database connection reference inside gin.Context
-	DB string = "DB_POOL"
-	// AppToken is a string used to hash passwords, extracted from environment variable
-	AppToken string = "APP_TOKEN"
 	// LoggedUser contains the UUID for the current user. It's set in AuthMidleware
 	LoggedUser string = "current-logged-user" // maybe inject as parameter in the future ?
 )
