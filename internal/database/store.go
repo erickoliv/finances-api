9fefee3d4c3ba505136bf4280d6946c472954a05
Erick Oliveira <erick.roberto@mercadolivre.com>
2020-09-27 18:03:08 -0300

    rename /auth/sql to /auth/authsql

--- internal/database/store.go
+++ internal/database/store.go
@@ -2,7 +2,7 @@ package database
 
 import (
 	"github.com/erickoliv/finances-api/accounts/accountsql"
-	"github.com/erickoliv/finances-api/auth/sql"
+	"github.com/erickoliv/finances-api/auth/authsql"
 	"github.com/erickoliv/finances-api/categories/categorysql"
 	"github.com/erickoliv/finances-api/entries/entrysql"
 	"github.com/erickoliv/finances-api/tags/tagsql"
@@ -10,7 +10,7 @@ import (
 )
 
 type SQLStore struct {
-	Auth     *sql.AuthRepo
+	Auth     *authsql.AuthRepo
 	Account  *accountsql.Repository
 	Category *categorysql.CategoryRepo
 	Tag      *tagsql.Repository
@@ -20,7 +20,7 @@ type SQLStore struct {
 func BuildSQLStore(db *gorm.DB) SQLStore {
 	return SQLStore{
 		Account:  accountsql.MakeAccounts(db),
-		Auth:     sql.MakeAuthenticator(db),
+		Auth:     authsql.MakeAuthenticator(db),
 		Category: categorysql.BuildRepository(db),
 		Tag:      tagsql.BuildTagRepository(db),
 		Entry:    entrysql.BuildRepository(db),
